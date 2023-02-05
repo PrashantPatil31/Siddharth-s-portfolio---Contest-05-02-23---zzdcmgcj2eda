9d051ac2851d0cf779719121740a885a8bd45b6a
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-05 21:08:00 +0530

    new

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { Link,Redirect } from "react-router-dom";
-import Info from './Info';
-import Home from './Home';
+import { Link } from "react-router-dom";
 const Blog = () => {
-  
-    const handleInfo=()=>{
-       Redirect("/Info");
-    }
-     const handleHome=()=>{
-       Redirect("/");
-    }
-
-    return (
-        <div id="blog-page">
-
-            <h1>Blog page</h1>
-            <h2>Checkout my awesome blogs </h2>
-            <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
-            <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <button id="blog-to-info" onClick={handleInfo} > Go to Info</button>
-            <button id="blog-to-home" onClick={handleHome} > Go to Home</button>
-        </div>
-    )
-}
-export default Blog
+  return (
+    <div id="blog-page">
+      <h1>Blog page</h1>
+      <h2>Checkout my awesome blogs </h2>
+      <h3>
+        <a href="https://www.newtonschool.co/post/top-13-web-developer-interview-questions">
+          Web Development Interview questions
+        </a>
+      </h3>
+      <h3>
+        <a href="https://www.newtonschool.co/post/graph-data-structure-explained-with-examples">
+          Graph Data Structures
+        </a>
+      </h3>
+      <button id="blog-to-info">
+        {" "}
+        <Link to="/Info"> Go to Info</Link>
+      </button>
+      <button id="blog-to-home">
+        {" "}
+        <Link to="/"> Go to Home</Link>
+      </button>
+    </div>
+  );
+};
+export default Blog;
