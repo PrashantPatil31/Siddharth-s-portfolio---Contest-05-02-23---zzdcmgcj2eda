0c94c89e6cfd3864c3c333f19e2f5ca5ebe100fd
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-05 20:47:34 +0530

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect} from 'react-router-dom'
+import Info from './Info';
+import Blog from './Blog';
 const Home = () => {
+   
+    const handleInfo=()=>{
+        Redirect("/Info");
+    }
+    const handleBlog=()=>{
+        Redirect("/Blog");
+    }
 
     return (
         <div id='home-page'>
@@ -8,8 +17,8 @@ const Home = () => {
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
+           <button id="home-to-blog"  onClick={handleBlog}> Go to Blog</button>
+            <button id="home-to-info"  onClick={handleInfo}> Go to Info</button>
 
         </div>
     )
