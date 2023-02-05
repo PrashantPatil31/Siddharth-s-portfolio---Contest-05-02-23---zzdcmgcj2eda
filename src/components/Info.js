8a74ce9d637efe5c20d63e78460307f64531f08f
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-05 21:09:23 +0530

    new

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,27 +1,20 @@
 import React from "react";
-import { Link , Redirect} from "react-router-dom";
-import Blog from './Blog';
-import Home from './Home';
+import { Link } from "react-router-dom";
 
 const Info = () => {
-
- const handleBlog=()=>{
-       Redirect("/Blog");
-    }
-  const handleHome=()=>{
-       Redirect("/");
-    }
-
-    return (
-        <div id="info-page">
-
-
-            <h1> Info page </h1>
-            <h2>I am born in India</h2>
-            <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"  onClick={handleBlog} > Go to Blog</button>
-            <button id="info-to-home"  onClick={handleHome} > Go to Home</button>
-        </div>
-    )
-}
-export default Info
+  return (
+    <div id="info-page">
+      <h1> Info page </h1>
+      <h2>I live in Gujarat</h2>
+      <h2>My name is Kevin </h2>
+      <button id="info-to-blog">
+        <Link to="/blog"> Go to Blog</Link>
+      </button>
+      <button id="info-to-home">
+        {" "}
+        <Link to="/">Go to Home</Link>
+      </button>
+    </div>
+  );
+};
+export default Info;
