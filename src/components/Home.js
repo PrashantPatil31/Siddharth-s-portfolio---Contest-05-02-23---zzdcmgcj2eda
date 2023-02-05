1753953589abfc27bb22523ea17b9ce12ca9aef0
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-05 21:08:40 +0530

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import { Link, Redirect} from 'react-router-dom'
-import Info from './Info';
-import Blog from './Blog';
+import React from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-   
-    const handleInfo=()=>{
-        Redirect("/Info");
-    }
-    const handleBlog=()=>{
-        Redirect("/Blog");
-    }
-
-    return (
-        <div id='home-page'>
-
-            <h1>Home page</h1>
-            <h2>Hi, my name is Siddharth </h2>
-            <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"  onClick={handleBlog}> Go to Blog</button>
-            <button id="home-to-info"  onClick={handleInfo}> Go to Info</button>
-
-        </div>
-    )
-}
-export default Home
+  return (
+    <div id="home-page">
+      <h1>Home page</h1>
+      <h2>Hi, my name is Kevin </h2>
+      <h2>
+        and I work at <a href="https://www.newtonschool.co/">Undefined</a>
+      </h2>
+      <button id="home-to-blog">
+        {" "}
+        <Link to="/blog"> Go to Blog</Link>
+      </button>
+      <button id="home-to-info">
+        {" "}
+        <Link to="/Info">Go to Info</Link>
+      </button>
+    </div>
+  );
+};
+export default Home;
